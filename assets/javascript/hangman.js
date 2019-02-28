8eaeafcde70f568fc6f7287f380687eb6d3610ea
Odysseus <arwaterman@gmail.com>
2019-02-28 15:48:46 -0800

    added in temple bell sound and added in comments to the code

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,3 +1,4 @@
+// words to randomly select for the game
 let words = [
     "banana",
     "mango",
@@ -12,81 +13,113 @@ let words = [
     "onomatopoeia"
 ];
 
-let intervalId;
-
+// the word to be selected
 let word = "";
+
+// index of the selected word
 let index = 0;
 
+// number of times the player has won the game
+let wins = 0;
+
+// the selected word, but all characters will be replaced with '_'
 let hiddenWord;
 
+// an array of all of the characters the user has selected durring the current session
 let lettersGuessed;
 
-// sets up the stats for hangman
+// used to keep track of the automatic timed reset after the game is finished
+let intervalId;
+
+// audio file to play when the user selects the correct letter
+let audio = new Audio("assets/sounds/temple-bell.mp3");
+
+// starts the game
 function initializeGame() {
+    // makes sure the game stops reseting
     clearInterval(intervalId);
     
+    // clears the hidden word and letters guessed during a previous session
     hiddenWord = [];
     lettersGuessed = [];
 
+    // retrieve the element that will display the hidden word
     let wordElement = document.getElementById("word");
 
+    // select a random word from the list
     index = Math.floor(Math.random() * words.length);
     word = words[index];
 
+    // push as many '_' characters as there are words in the chosen word
     for(let i in word.split("")) {
         hiddenWord.push("_");
     }
 
+    // display the hidden word to the screen
     wordElement.textContent = hiddenWord.join(" ");
 
+    // re-initializes the game text on the main web page
     document.getElementById("guessed-letters").textContent = "";
     document.getElementById("win-text").textContent = "";
     document.getElementById("new-game-text").textContent = "";
+    document.getElementById("win-score").textContent = "wins: " + wins;
 }
 
 // renders the word in the browser after every key press
 document.addEventListener("keyup", event => {
-    console.log(event.key);
 
+    // normalizes the input to lower case
     let eventKey = event.key.toLowerCase();
 
-    // the letter has not been guessed
+    // determines if the key pressed was part of the alphabet, and if the key has not been guessed yet, otherwise we ignore the key event
     if (eventKey.length === 1 && isAlpha(eventKey) && lettersGuessed.indexOf(eventKey) < 0) {
 
+        // push the key to the list of guessed letters
         lettersGuessed.push(eventKey);
         console.log("pushed key " + eventKey + " to the letters guessed array");
+
+        // display the guessed letters to the web page
         document.getElementById("guessed-letters").textContent = lettersGuessed.join(" ");
 
+        // determines if the key pressed is in the word
         if (word.indexOf(eventKey) > -1) {
+
+            // plays the sound of a temple bell on a successful guess
+            audio.currentTime = 0;
+            audio.play();
     
+            // displays the letter guessed within the hidden word and displays it on the web page
             for (let i in word) {
                 if (word.charAt(i) === eventKey) {
                     hiddenWord[i] = word.charAt(i);
-                    console.log("index " + i + " character " + word.charAt(i));
                 }
             }
-    
             document.getElementById("word").textContent = hiddenWord.join(" ");
 
+            // determines if the game is over
             if (hiddenWord.indexOf("_") < 0) {
+
+                // tally the win
+                wins++;
+
+                // congratulate the user
                 document.getElementById("win-text").textContent = "Congratulations, you win!";
 
+                // inform them that a new game will be starting soon
                 document.getElementById("new-game-text").textContent = "A new game will start in just a moment";
+                
+                // start the game in 5 seconds
                 intervalId = setInterval(initializeGame, 5 * 1000);
             }
         }
     }
 
-    
 });
 
+// determines if the first letter of the string is in the alphabet
 function isAlpha(letter) {
     return /[a-zA-Z]/.test(letter);
 }
 
+// starts the game!
 initializeGame();
-// console.log(words);
-// console.log(index);
-// console.log(word);
-
-// render the game
